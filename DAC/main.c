fff06253fd5f294b383ebffd31b0cf18630790ae
andrewchandlerramirez <andrewchandlerramirez@gmail.com>
2019-02-13 11:17:44 -0800

    blank2

--- DAC/main.c
+++ DAC/main.c
@@ -1,88 +1,8 @@
-// This is your first program to run on the LaunchPad
-// You will run this program without modification as your Lab 2
-// If the left switch SW1 is 
-//      not pressed the LED toggles blue-red
-//      pressed the LED toggles blue-green
-
-// 0.Documentation Section 
-// main.c
-// Runs on LM4F120 or TM4C123
-// Lab2_HelloLaunchPad, Input from PF4, output to PF3,PF2,PF1 (LED)
-// Authors: Daniel Valvano, Jonathan Valvano and Ramesh Yerraballi
-// Date: December 28, 2014
-
-// LaunchPad built-in hardware
-// SW1 left switch is negative logic PF4 on the Launchpad
-// SW2 right switch is negative logic PF0 on the Launchpad
-// red LED connected to PF1 on the Launchpad
-// blue LED connected to PF2 on the Launchpad
-// green LED connected to PF3 on the Launchpad
-
-// 1. Pre-processor Directives Section
-// Constant declarations to access port registers using 
-// symbolic names instead of addresses
-// TExaS.o is the object code for the real-board grader
-#include "TExaS.h"
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-
-// 2. Declarations Section
-//   Global Variables
-unsigned long In;  // input from PF4
-unsigned long Out; // outputs to PF3,PF2,PF1 (multicolor LED)
-
-//   Function Prototypes
-void PortF_Init(void);
-void Delay(void);
-void EnableInterrupts(void);
 
 
-// 3. Subroutines Section
-// MAIN: Mandatory for a C Program to be executable
-int main(void){    
-  TExaS_Init(SW_PIN_PF40,LED_PIN_PF321); 
-  // TExaS_Init initializes the real-board grader for lab 2
-  PortF_Init();        // Call initialization of port PF4 PF2    
-  EnableInterrupts();  // The grader uses interrupts
-  while(1){
-    In = GPIO_PORTF_DATA_R&0x10; // read PF4 into In
-    if(In == 0x00){              // zero means SW1 is pressed
-      GPIO_PORTF_DATA_R = 0x08;  // LED is green
-    } else{                      // 0x10 means SW1 is not pressed
-      GPIO_PORTF_DATA_R = 0x02;  // LED is red
-    }
-    Delay();                     // wait 0.1 sec
-    GPIO_PORTF_DATA_R = 0x04;    // LED is blue
-    Delay();                     // wait 0.1 sec
-  }
-}
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
 
-// Subroutine to initialize port F pins for input and output
-// PF4 and PF0 are input SW1 and SW2 respectively
-// PF3,PF2,PF1 are outputs to the LED
-// Inputs: None
-// Outputs: None
-// Notes: These five pins are connected to hardware on the LaunchPad
-void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
-  delay = SYSCTL_RCGC2_R;           // delay   
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0       
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
-  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0        
-}
 // Color    LED(s) PortF
 // dark     ---    0
 // red      R--    0x02
@@ -93,13 +13,109 @@ void PortF_Init(void){ volatile unsigned long delay;
 // white    RGB    0x0E
 // pink     R-B    0x06
 
-// Subroutine to wait about 0.1 sec
-// Inputs: None
-// Outputs: None
-// Notes: the Keil simulation runs a little slower than the real board
-void Delay(void){unsigned long volatile time;
-  time = 727240*200/91;  // 0.1sec
-  while(time){
-    time--;
-  }
+void sawtooth(void){
+	
+	GPIO_PORTB_DATA_R ^=0x01;// (GPIO_PORTB_DATA_R == 255)? 0: GPIO_PORTB_DATA_R + 1;
+	
+}
+
+void PortB_Init(void){volatile unsigned long delay;
+
+	SYSCTL_RCGC2_R |= 0x02;//activating port b clock
+	delay = SYSCTL_RCGC2_R;//delay
+	GPIO_PORTB_CR_R |= 0xFF;//allows changes to all bits of port B
+	GPIO_PORTB_AMSEL_R &= ~0xFF;////Disables Analog functionality on all of port b
+	GPIO_PORTB_PCTL_R &= 0x00000000;//All pins are set up for GPIO
+	GPIO_PORTB_DIR_R |= 0xFF;//All pins are output
+	GPIO_PORTB_AFSEL_R &= ~0xFF;//All pins are not using alternate function
+	GPIO_PORTB_PUR_R |= 0xFF;//
+	GPIO_PORTB_DEN_R |= 0xFF;//All pins are digitaly enabled
+	//initializing port b
+	GPIO_PORTB_DATA_R &= ~0xFF;//Clears all bit on Port B
+	
+}
+
+void Button_Init(void){volatile unsigned long delay;//delay
+	SYSCTL_RCGC2_R |= 0x20;//clock to portF
+	delay = SYSCTL_RCGC2_R;//delay
+	GPIO_PORTF_LOCK_R |= 0x4C4F434B;
+	GPIO_PORTF_CR_R |= 0x11;//allows changes to PF0 and PF4
+	GPIO_PORTF_AMSEL_R &= ~0x11;//disables analog functionality to PF0 and PF4
+	GPIO_PORTF_PCTL_R &= ~0x000F000F;//GPIO Functionality to PF0 and PF4
+	GPIO_PORTF_DIR_R |= ~0x11;
+	GPIO_PORTF_AFSEL_R &= ~0x11;//disables alternate functionalit to PF0 and PF4
+	GPIO_PORTF_PUR_R |= 0x11;
+	GPIO_PORTF_DEN_R |= 0x11;
+	
+	GPIO_PORTF_IS_R &= ~0x11;//PF0 and PF4 edge triggerd
+	GPIO_PORTF_IBE_R &= ~0x11;//Not triggered on both edges
+	GPIO_PORTF_IEV_R &= ~0x11;//faling edge triggered
+	GPIO_PORTF_ICR_R = 0x11;//clear flags
+	GPIO_PORTF_IM_R = 0x11;//arm interrupt
+	
+	NVIC_PRI7_R = (NVIC_PRI7_R & ~0x00E00000)|0x00400000;
+	NVIC_EN0_R = 0x40000000;
+}
+
+void LED_Init(void){volatile unsigned long delay;
+	SYSCTL_RCGC2_R |= 0x20;//activate clock for port F
+	delay = SYSCTL_RCGC2_R;//delay
+	GPIO_PORTF_LOCK_R |= 0x4C4F434B;//unlock
+	GPIO_PORTF_CR_R |= 0x0E;//allow changes
+	GPIO_PORTF_AMSEL_R &= ~0x0E;//disable analog functionality
+	GPIO_PORTF_PCTL_R &= ~0x0000FFF0;//configure to GPIO
+	GPIO_PORTF_DIR_R |= 0x0E;//output
+	GPIO_PORTF_AFSEL_R &= ~0x0E;//disable alt function
+	GPIO_PORTF_PUR_R |= 0x0E;//
+	GPIO_PORTF_DEN_R |= 0x0E;//enable digitle I/O
+	
+	GPIO_PORTF_DATA_R |= 0x02;//make red initially
+	
+}
+
+void GPIOPortF_Handler(void){
+	
+
+	if(GPIO_PORTF_RIS_R&0x01){//if PF0    SW2	
+		GPIO_PORTF_ICR_R = 0x01;
+		////////////////
+		
+	}
+	
+	if(GPIO_PORTF_RIS_R&0x10){//if PF4		SW1
+		GPIO_PORTF_ICR_R = 0x10;
+		//////////////////////////
+	}
+
+}
+void Timer1_Init(unsigned long period){
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER1_TAILR_R = period-1;    // 4) reload value
+  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER1A timeout flag
+  TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF00FF)|0x00008000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 37, interrupt number 21
+  NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
+  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
+}
+
+void Timer1A_Handler(void){
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
+	sawtooth();
+	
+	}
+
+
+int main(void){
+	//Button_Init();
+	//LED_Init();
+	Timer1_Init(8000000);
+	
+	
+	while(1);
 }
