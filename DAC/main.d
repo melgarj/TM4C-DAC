c082abf32acd16d4bb41100c02c8396da1afefb3
andrewchandlerramirez <andrewchandlerramirez@gmail.com>
2019-02-25 11:17:15 -0800

    Added Sine Wave stuff - not really

--- DAC/main.d
+++ DAC/main.d
@@ -1,3 +1,5 @@
 .\main.o: main.c
 .\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
 .\main.o: tm4c123gh6pm.h
+.\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\math.h
